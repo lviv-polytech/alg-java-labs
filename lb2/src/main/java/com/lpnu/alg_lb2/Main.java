47ef409090e9d3f9fc01b3574bbda8e3e5f8905a
Mykyta <c.kryvobokov.mykyta@student.uzhnu.edu.ua>
2026-09-19 22:36:19 +0300

    refactor(io): move InputLine contract and console implementation to common-utils; update lb2 to use common IO factory

--- lb2/src/main/java/com/lpnu/alg_lb2/Main.java
+++ lb2/src/main/java/com/lpnu/alg_lb2/Main.java
@@ -2,7 +2,7 @@ package com.lpnu.alg_lb2;
 
 import com.lpnu.alg_lb2.dto.Lb2ExpressionResult;
 import com.lpnu.common.dto.InParams;
-import com.lpnu.alg_lb2.util.InputLineFactory;
+import com.lpnu.common.io.InputLineFactory;
 import com.lpnu.alg_lb2.var.SevenTeen;
 
 /** Entry point for the laboratory assignment application. */
