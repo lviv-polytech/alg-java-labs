fda16d44d70a7bb016405713df05546805f71442
Mykyta <c.kryvobokov.mykyta@student.uzhnu.edu.ua>
2026-09-19 22:30:59 +0300

    refactor(lb3): remove module-info (avoid JPMS reactor resolution issues); keep organizational JavaDoc improvements

--- lb3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.lpnu.lb3 {
-  requires module.lb2;
-  requires com.lpnu.commonutils;
-
-  // Export only top-level API packages; keep implementation packages internal to the module
-  exports com.lpnu.alg_lb3;
-  exports com.lpnu.alg_lb3.util;
-}
