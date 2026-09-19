b13e9dd579bb7129e7aea8436e9972e61d3ddc02
Mykyta <c.kryvobokov.mykyta@student.uzhnu.edu.ua>
2026-09-19 22:37:21 +0300

    refactor(common): remove lb2 local DTOs and use common DTOs; update Lb2ExpressionResult to implement common ExpressionResult

--- lb2/src/main/java/com/lpnu/alg_lb2/dto/ExpressionResult.java
+++
@@ -1,4 +0,0 @@
-package com.lpnu.alg_lb2.dto;
-
-/** Contract for a variant result that exposes both computed values. */
-public interface ExpressionResult extends com.lpnu.common.dto.ExpressionResult {}
